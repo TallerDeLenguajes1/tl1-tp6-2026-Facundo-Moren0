1f5b6efa30a30f33e14cc897757192b6c8864d6d
Facundo-Moren0 <FacundoMoreno451@gmail.com>
2026-06-07 23:17:50 -0300

    Ejercicio 1

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿
+Console.WriteLine("Ingresar un numero a invertir");
+string numeroString = Console.ReadLine();
+
+int numero = 0;
+string numeroResult = "";
+
+
+
+bool esNum = int.TryParse(numeroString, out numero);
+
+if(esNum && numero > 0){
+    while(numero != 0){
+        numeroResult += (numero % 10);
+        numero = (numero / 10);        
+    }
+
+    Console.Write($"El numero invertido es {numeroResult}");
+
+}
